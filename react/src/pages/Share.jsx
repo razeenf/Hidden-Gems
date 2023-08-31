7e0266210845a407903945a45f15f9063b39e78e
razeenf <razeenfaruque@gmail.com>
2023-08-31 01:54:47 -0400

    axios length config

--- react/src/pages/Share.jsx
+++ react/src/pages/Share.jsx
@@ -32,8 +32,13 @@ export default function FormPage() {
     formData.append('description', description);
     formData.append('image', imageFile);
 
+    const axiosConfig = {
+      maxContentLength: 10000000, 
+      maxBodyLength: 10000000,   
+    };
+
     // API call to the POST /posts endpoint
-    await axios.post("/api/posts", formData, { headers: {'Content-Type': 'multipart/form-data'}})
+    await axios.post("/api/posts", formData, { headers: {'Content-Type': 'multipart/form-data'}}, axiosConfig)
       .then(response => {
         if (response.status === 201) {
           setSubmitted(true);
